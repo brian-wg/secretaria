6e9186a2bc9899c047afd28a510eea1d75550bc5
Brian <brian_wg@hotmail.com>
2023-09-24 22:06:47 -0300

    Se modifican detalles varios para la correcta funcionalidad general y se agrega el archivo para crear una persona y estilos

--- clases/ControladorSesion.php
+++ clases/ControladorSesion.php
@@ -1,6 +1,6 @@
 <?php 
 require_once 'RepositorioPersona.php';
-require_once 'RepositorioLicencias.php';
+require_once 'RepositorioLicencia.php';
 require_once 'Persona.php';
 require_once 'Licencia.php';
 
@@ -22,7 +22,22 @@ class ControladorSesion{
 			return [true, "Ingreso correcto"];
 		}
 	}
+
+	public function create($nombre, $apellido, $dni, $email, $telefono,
+	$domicilio, $situacion_revista, $fecha_toma_posicion, $clave, $rol){
+
+		$r = new RepositorioPersona();
+		$persona = new Persona($nombre, $apellido, $dni, $email, $telefono, $domicilio, $situacion_revista, $fecha_toma_posicion, $rol);
+		$id = $r->save($persona, $clave);
+		if ($id ===false) {
+			return [false, "No se pudo crear el usuario"];
+		} 
+		else {
+			$persona->setIdPersona($id);
+			session_start();
+			$_SESSION['email'] = serialize($persona);
+			return [true, "Usuario creado con exito!"];
+		}
+	}
 }
-
-
  ?>
--- clases/RepositorioLicencia.php
+++ clases/RepositorioLicencia.php
@@ -81,3 +81,4 @@ private static $conexion = null;
             return false;
         }
     }
+}
